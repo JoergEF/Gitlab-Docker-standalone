665cd3308a4721140509f8ce69e5d9939717f3a8
Jörg Hofmann <joerg@thuenic.de>
2020-12-09 17:34:10 +0100

    Provision fertig

--- scripts/gitlab-provision.sh
+++ scripts/gitlab-provision.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-# Hier kommt Bockwurst rein
+# allgemeine Updates
 apt-get update
 apt-get -y upgrade
+# Installation der aktuellen Docker-CE
 apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
@@ -9,13 +10,14 @@ apt-get update
 apt-get -y install docker-ce docker-ce-cli containerd.io
 usermod -aG docker vagrant
 systemctl enable docker
+# nötige Verzeichnisse erstellen
 mkdir -p /srv/gitlab/{data,logs,config}
+# GitLab-CE Container erstellen
 docker run --detach \
 --hostname gitlab \
---publish 443:443 --publish 80:80 \
+--publish 80:80 \
 --name gitlab \
 --restart always \
---env EXTERNAL_URL=https://gitlab \
 --volume /srv/gitlab/config:/etc/gitlab \
 --volume /srv/gitlab/logs:/var/log/gitlab \
 --volume /srv/gitlab/data:/var/opt/gitlab \
