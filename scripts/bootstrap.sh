a0884215a56c0b78c5ed6c67485cc2bcdc9f3e5e
= <=>
2022-03-31 10:38:48 +0200

    auf docker-compose umgestellt
    wegen gitlab-Container-Registry

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -7,7 +7,7 @@ apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
-apt-get -y install docker-ce docker-ce-cli containerd.io
+apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose
 usermod -aG docker vagrant
 systemctl enable docker
-mkdir -p /srv/gitlab
+mkdir -p /srv/gitlab/{config,logs,data}
--- scripts/gitlab-provision.sh
+++ scripts/gitlab-provision.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 # GitLab-CE Container erstellen
-docker run --detach \
---hostname gitlab \
---publish 80:80 \
---name gitlab \
---restart always \
---shm-size 256m \
-gitlab/gitlab-ce:latest
+docker compose up -d
+
