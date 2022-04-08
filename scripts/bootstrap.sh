06073429cf62af0d55ece72b1cff0462dcac26a2
Jörg Hofmann <joerg@thuenic.de>
2022-04-08 08:18:48 +0200

    dnsmasq eingebaut

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
+
 # apt im Noninteractive-Modus
 export DEBIAN_FRONTEND=noninteractive
+
 # allgemeine Updates
 apt-get update
 apt-get -y upgrade
+
+# Konfiguration der Namensauflösung
+systemctl disable --now systemd-resolved
+rm /etc/resolv.conf
+echo "nameserver 10.50.100.10" > /etc/resolv.conf
+echo "nameserver 10.50.100.11" >> /etc/resolv.conf
+echo "search kurs.iad" >> /etc/resolv.conf
+apt-get -y install dnsmasq
+
 # Installation der aktuellen Docker-CE
 apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
 apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose
-usermod -aG docker vagrant
 systemctl enable docker
+
+# Docker-Rechte für Vagrant
+usermod -aG docker vagrant
+
 mkdir -p /srv/gitlab/{config,logs,data}
 cp /vagrant/config/certs/* /srv/gitlab/config
 
